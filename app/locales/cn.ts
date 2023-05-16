e55074a6c261af0059a8e16e3778ea84d6d87d73
xiongbubai <132990397+xiongbubai@users.noreply.github.com>
2023-05-16 21:17:36 +0800

    Update cn.ts

--- app/locales/cn.ts
+++ app/locales/cn.ts
@@ -4,7 +4,7 @@ const cn = {
   WIP: "该功能仍在开发中……",
   Error: {
     Unauthorized:
-      "现在是未授权状态，点击左下角[设置](/#/settings)按钮输入访问密码，没有访问密码请点击[这里](http://fk.xbbwk.top)获取。",
+      "现在是未授权状态，点击左下角[设置](/#/settings)按钮输入访问密码，没有访问密码请联系微信czi713获取。",
   },
   ChatItem: {
     ChatItemCount: (count: number) => `${count} 条对话`,
